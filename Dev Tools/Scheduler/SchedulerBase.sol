241ee466dc52b9aabffd3067f2ef93fe614d6b59
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-05-31 17:04:57 -0700

    Update SchedulerBase.sol

--- Dev Tools/Scheduler/SchedulerBase.sol
+++ Dev Tools/Scheduler/SchedulerBase.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 contract SchedulerBase {
 
     event JobCreated (bytes32 ID, address Owner, 
-        address Validator, uint256 Timestamp
+        address Validator, string Schedule, JobTypes Type, uint256 Timestamp
     );
 
     event JobFulfilled (bytes32 ID, address Validator, 
@@ -42,9 +42,16 @@ contract SchedulerBase {
     }
 
     bytes32[] public JobIDs;
-
     mapping (bytes32 => Job) public IDToJob;
     mapping (address => uint256) public Subscription;
 
-   
+    //If you want to skip a value input 9999: ------------------------
+
+    //Year | Month | Date | Hour | Minute | Second
+    uint256[] public DateLB = [2023, 0, 1, 0, 0, 0]; 
+    uint256[] public DateUB = [9998, 11, 31, 23, 59, 59];
+
+    //Year | Month | Date | Day of Week | Hour | Min | Second
+    uint256[] public ReccurenceLB = [2023, 0, 1, 0, 0, 0, 0];
+    uint256[] public ReccurenceUB = [9998, 11, 31, 6, 23, 59, 59];
 }
