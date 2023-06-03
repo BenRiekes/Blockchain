de0c853994c0b539d7c6c61988690a753afe9254
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-06-02 22:45:40 -0700

    Update Scheduler.sol

--- Dev Tools/Scheduler/Scheduler.sol
+++ Dev Tools/Scheduler/Scheduler.sol
@@ -3,235 +3,227 @@
 pragma solidity ^0.8.20;
 
 import "./SchedulerBase.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
 
 contract Scheduler is SchedulerBase {
 
-    //Access Control: -----------------------------------------------
-   
     modifier Auth (bytes32 _jobID, bool _onlyOwner) {
 
         require (IDToJob[_jobID].ID == _jobID && IDToJob[_jobID].isActive,
-            "Invalid ID or query for inactive Job"
+            "Invalid ID or query for inactive job"
         );
 
         if (_onlyOwner) {
-            require (IDToJob[_jobID].owner == msg.sender);
+            require (IDToJob[_jobID].owner == msg.sender, "Unauthorized");
         }
         _;
     }
 
-    //Info Getters: ------------------------------------------------
+    //Getter Functions: -------------------------------------------------------------------
 
-    function getFulfillment (bytes32 _jobID, uint256 _index) external view returns (bool[] memory success, bytes[] memory responses, uint256 timestamp) {
-
-        require (IDToJob[_jobID].fulfillments.length > _index, 
-            "Index out of bounds"
-        ); 
+    function getFulfillment (bytes32 _jobID, uint256 _index) external view returns (bool[] memory success, bytes[] memory resonse, uint256 timestamp) {
 
         Fulfillment memory fulfillment = IDToJob[_jobID].fulfillments[_index];
+        return (fulfillment.success, fulfillment.response, fulfillment.timestamp); 
+    }   
 
-        return (fulfillment.success, fulfillment.response, fulfillment.timestamp);
-    }
-
-    function getCall (bytes32 _jobID, uint256 _index) external view returns (bytes memory data, address target, uint256 funding) {
-
-        require (IDToJob[_jobID].calls.length > _index, 
-            "Index out of bounds"
-        );
+    function getCall (bytes32 _jobID, uint256 _index) external view returns (bytes memory data, address target, uint256 reqfunds) {
 
         Call memory call = IDToJob[_jobID].calls[_index];
+        return (call.data, call.target, call.reqfunds);
+    } 
 
-        return (call.data, call.target, call.funding);
+    function getJobSchedule (bytes32 _jobID) external view returns (uint256 num, uint256[] memory numArr, string memory str) {
+        
+        return decodeSchedule(IDToJob[_jobID].schedule, IDToJob[_jobID].jobType);
     }
 
-    function getJobFunding (bytes32 _jobID) public view returns (uint256 funding) {
+    function getJobFunding (bytes32 _jobID) public view returns (uint256 reqfunds) {
 
         for (uint i = 0; i < IDToJob[_jobID].calls.length; i++) {
-            funding += IDToJob[_jobID].calls[i].funding;
-        }
-    }
-
-    //Helper Functions: -------------------------------------------------
-
-    function buildSchedule (uint256[] memory _dates) internal pure returns (string memory) {
-
-        string memory schedule = "";
-
-        for (uint i = 0; i < _dates.length; i++) {
-
-            schedule = string(abi.encodePacked(
-               schedule, 
-               i == 0 ? "" : ", ", 
-               Strings.toString(_dates[i])
-            ));
+            reqfunds += IDToJob[_jobID].calls[i].reqfunds;
         }
 
-        return schedule;
+        return reqfunds;
+    }
+    
+    //Helper Functions: -------------------------------------------------------------------
+
+    function decodeSchedule (bytes memory _schedule, JobTypes _type) internal pure returns (uint256 num, uint256[] memory numArr, string memory str) {
+        
+        str = _type == JobTypes.Cron ? 
+            (string(_schedule)) : ("")
+        ;
+
+        numArr = _type == JobTypes.Date || _type == JobTypes.Recurrence ? 
+            (abi.decode(_schedule, (uint256[]))) : (new uint256[](0))
+        ; 
+
+        num = _type == JobTypes.Blocktime ? 
+            (abi.decode(_schedule, (uint256))) : (0)
+        ; 
+
+        return (num, numArr, str);
     }
 
-    function verifyBounds (uint256[] memory _dates, uint256[] memory _lower, uint256[] memory _upper) internal pure returns (bool) {
 
-        require (_dates.length == _lower.length && _lower.length == _upper.length,
-            "Invalid parameters"
-        ); 
+    function verifySchedule (bytes memory _schedule, JobTypes _type) internal view returns (bool verified) {
 
-        for (uint i = 0; i < _dates.length; i++) {
+        (uint256 num, uint256[] memory numArr,) = decodeSchedule(
+            _schedule, _type
+        );
 
-            if (_dates[i] < _lower[i] || _dates[i] > _upper[i]) {
-                return false;
+        if (_type == JobTypes.Cron) {
+            verified = _schedule.length >= 5;
+
+        } else if (_type == JobTypes.Blocktime) {
+            verified = num > block.timestamp;
+
+        } else { verified = true;
+            
+            (uint256[] memory LB, uint256[] memory UB) = _type == JobTypes.Date ? 
+                (DateLB, DateUB) : (ReccurenceLB, ReccurenceUB)
+            ;
+
+            for (uint i = 0; i < numArr.length; i++) {
+
+                if (numArr[i] < LB[i] || numArr[i] > UB[i]) {
+                    verified = false;
+                }
             }
         }
 
-        return true;
-    }   
-
-    function verifySchedule (bytes memory _schedule, JobTypes _type) internal view returns (bool verified, string memory schedule) {
-
-        if (_type == JobTypes.Cron) {
-            verified = _schedule.length >= 5; 
-            schedule = string(_schedule);
-        }
-
-        else if (_type == JobTypes.Date) {
-            uint256[] memory arr = abi.decode(_schedule, (uint256[]));
-            verified = verifyBounds(arr, DateLB, DateUB);
-            schedule = buildSchedule(arr);
-        }
-
-        else if (_type == JobTypes.Recurrence) {
-            uint256[] memory arr = abi.decode(_schedule, (uint256[]));
-            verified = verifyBounds (arr, ReccurenceLB, ReccurenceUB);
-            schedule = buildSchedule(arr);
-        }
-
-        else if (_type == JobTypes.Blocktime) {
-            uint256 blocktime = abi.decode(_schedule, (uint256)); 
-            verified = blocktime > block.timestamp;
-            schedule = Strings.toString(blocktime);
-        }
-    } 
-
-    //Primary Functions: ------------------------------------------------
-
-    function createJob (Call[] memory _calls, bytes memory _schedule, JobTypes _type, address _validator) external returns (bytes32 ID) {
-
-        (bool verified, string memory schedule) = verifySchedule(
-            _schedule, _type
-        ); 
-
-        require (verified, "Incorrect schedule or job type"); 
-        require (_validator != address(this), "Invalid validator"); 
+        return verified;
+    }
     
+    //Primary Functions: -------------------------------------------------------------------
+
+    function scheduleJob (Call[] memory _calls, bytes memory _schedule, JobTypes _type, address _validator) external returns (bytes32 ID) {
+
+        require (address(this) != _validator, "Invalid validator");
+        require (verifySchedule(_schedule, _type), "Invalid schedule"); 
+
         ID = keccak256(abi.encodePacked(
             block.timestamp, block.number, block.prevrandao, msg.sender
         ));
 
         IDToJob[ID].ID = ID;
-        IDToJob[ID].isActive = true;
         IDToJob[ID].owner = msg.sender;
         IDToJob[ID].validator = _validator;
-
+        IDToJob[ID].schedule = _schedule;
         IDToJob[ID].jobType = _type;
-        IDToJob[ID].schedule = schedule;
-        
+        IDToJob[ID].isActive = true;
+
         for (uint i = 0; i < _calls.length; i++) {
-            addCall(ID, _calls[i]);
+            addCall(ID, _calls[i]); 
         }
 
-        emit JobCreated (ID, msg.sender, _validator, schedule, _type, block.timestamp); 
-    }
+        JobIDs.push(ID);
+        emit JobCreated (ID, _validator, _schedule, _type, block.timestamp);
+    }   
 
     function fulfillJob (bytes32 _jobID) external Auth (_jobID, false) {
 
-        Job memory job = IDToJob[_jobID];
+        require (IDToJob[_jobID].validator == msg.sender,
+            "Unauthorized"
+        );
 
-        require (job.validator == msg.sender, 
-            "Unauthorized validator"    
-        ); 
+        require (Subscription[IDToJob[_jobID].owner] >= getJobFunding(_jobID),
+            "Insufficient funds"
+        );
 
-        require (address(this).balance >= getJobFunding(_jobID) && Subscription[job.owner] >= getJobFunding(_jobID),
-            "Insufficient contract or subscription balance"
-        ); 
-
-        bool[] memory successes = new bool[](job.calls.length);
-        bytes[] memory responses = new bytes[](job.calls.length);
+        Job memory job = IDToJob[_jobID]; 
+        bool[] memory success = new bool[](job.calls.length);
+        bytes[] memory response = new bytes[](job.calls.length);
 
         for (uint i = 0; i < job.calls.length; i++) {
 
-            (bool success, bytes memory response) = payable(job.calls[i].target).call{
-                value: job.calls[i].funding
+            Call memory call = job.calls[i];
 
-            }(job.calls[i].data);
+            (bool s, bytes memory r) = payable(call.target).call{
+                value: call.reqfunds
+            }(call.data);
 
-            if (success) {
-                Subscription[job.owner] -= job.calls[i].funding;
+            if (s && call.reqfunds > 0) {
+                Subscription[job.owner] -= call.reqfunds;
             }
 
-            successes[i] = success;
-            responses[i] = response;
+            success[i] = s;
+            response[i] = r;
         }
 
         IDToJob[_jobID].fulfillments.push(Fulfillment({
-            success: successes,
-            response: responses,
+            success: success,
+            response: response,
             timestamp: block.timestamp
         })); 
 
-        emit JobFulfilled (_jobID, msg.sender, successes, responses, block.timestamp);
-    } 
+        emit JobFulfilled (_jobID, msg.sender, success, response, block.timestamp);
+    }
 
-    //Secondary Functions ------------------------------------------------
+    //Secondary Functions: -------------------------------------------------------------------
 
-    function addCall (bytes32 _jobID, Call memory call) public Auth (_jobID, true) {
+    function addCall (bytes32 _jobID, Call memory _call) public Auth (_jobID, true) {
 
-        require (call.target != address(this), 
-            "Invalid target"
-        ); 
+        require (address(this) != _call.target, 
+            "Invalid target address"
+        );
 
-        IDToJob[_jobID].calls.push(call);
+        IDToJob[_jobID].calls.push(_call);
     }
 
     function removeCall (bytes32 _jobID, uint256 _index) external Auth (_jobID, true) {
 
-        require (IDToJob[_jobID].calls.length > _index, 
-            "Index out of bounds"
-        ); 
+        uint256 topIndex = IDToJob[_jobID].calls.length - 1;
 
-        IDToJob[_jobID].calls[_index] = IDToJob[_jobID].calls
-            [IDToJob[_jobID].calls.length - 1]
-        ; 
+        // If the index to remove is not the last one, swap it with the last one
+        if (_index != topIndex) {
+            IDToJob[_jobID].calls[_index] = IDToJob[_jobID].calls[topIndex];
+        }
 
         IDToJob[_jobID].calls.pop();
     }
 
-    //Setters: ------------------------------------------------------------
-
-    function deleteJob (bytes32 _jobID) external Auth (_jobID, true) {
-        IDToJob[_jobID].isActive = false; 
-    }
+    //Setter Functions: -------------------------------------------------------------------
 
     function setValidator (bytes32 _jobID, address _validator) external Auth (_jobID, true) {
-        IDToJob[_jobID].validator = _validator; 
+        
+        require (address(this) != _validator, "Invalid validator");
+        IDToJob[_jobID].validator = _validator;
     }
 
-    //Financial: ------------------------------------------------------------
+    function deleteJob (bytes32 _jobID) external Auth (_jobID, true) { 
 
-    function deposit (address _reciever) external payable {
-        Subscription[_reciever] += msg.value;
+        uint256 counter;
+        bytes32[] memory IDs = new bytes32[] (JobIDs.length - 1);
+
+        for (uint i = 0; i < JobIDs.length; i++) {
+
+            if (JobIDs[i] != _jobID) {
+                IDs[counter] = JobIDs[i];
+                counter++;
+            }
+        }
+
+        JobIDs = IDs;
+        IDToJob[_jobID].isActive = false;
+    }
+
+    //Financial Functions: -------------------------------------------------------------------
+
+    function deposit (address _subscriber) external payable {
+        Subscription[_subscriber] += msg.value;
     }
 
     function withdraw (uint256 _amount) external {
 
-        require (Subscription[msg.sender] >= _amount && address(this).balance > _amount, 
-            "Insufficient balance"
-        );
+        require (Subscription[msg.sender] >= _amount, "Insufficient funds");
 
         Subscription[msg.sender] -= _amount;
 
-        (bool success, ) = payable(msg.sender).call{
+        (bool s, ) = payable(msg.sender).call{
             value: _amount
+        }("");
 
-        }(""); require (success, "Transfer failed"); 
+        require (s, "Withdrawl failed");
     }
 }
