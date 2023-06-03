d83288b714318f69e09127e4948ca9efbaaf6d88
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-06-02 22:46:18 -0700

    Update SchedulerBase.sol

--- Dev Tools/Scheduler/SchedulerBase.sol
+++ Dev Tools/Scheduler/SchedulerBase.sol
@@ -4,22 +4,20 @@ pragma solidity ^0.8.20;
 
 contract SchedulerBase {
 
-    event JobCreated (bytes32 ID, address Owner, 
-        address Validator, string Schedule, JobTypes Type, uint256 Timestamp
+    event JobCreated (bytes32 indexed ID, address indexed Validator, 
+        bytes Schedule, JobTypes Type, uint256 Timestamp
     );
 
-    event JobFulfilled (bytes32 ID, address Validator, 
-        bool[] Success, bytes[] Data, uint256 Timestamp
-    );
+    event JobFulfilled (bytes32 indexed ID, address indexed Validator, 
+        bool[] Success, bytes[] Respons, uint256 Timestamp
+    ); 
 
-    //------------------------------------------------
-
-    enum JobTypes {Undeclared, Cron, Date, Recurrence, Blocktime}
+    enum JobTypes {Cron, Date, Recurrence, Blocktime}
 
     struct Call {
         bytes data;
         address target;
-        uint256 funding;
+        uint256 reqfunds;
     }
 
     struct Fulfillment {
@@ -32,8 +30,8 @@ contract SchedulerBase {
         bytes32 ID;
         address owner;
         address validator;
+        bytes schedule;
 
-        string schedule;
         bool isActive;
         JobTypes jobType;
 
@@ -48,10 +46,10 @@ contract SchedulerBase {
     //If you want to skip a value input 9999: ------------------------
 
     //Year | Month | Date | Hour | Minute | Second
-    uint256[] public DateLB = [2023, 0, 1, 0, 0, 0]; 
-    uint256[] public DateUB = [9998, 11, 31, 23, 59, 59];
+    uint256[] internal DateLB = [2023, 0, 1, 0, 0, 0]; 
+    uint256[] internal DateUB = [9998, 11, 31, 23, 59, 59];
 
     //Year | Month | Date | Day of Week | Hour | Min | Second
-    uint256[] public ReccurenceLB = [2023, 0, 1, 0, 0, 0, 0];
-    uint256[] public ReccurenceUB = [9998, 11, 31, 6, 23, 59, 59];
+    uint256[] internal ReccurenceLB = [2023, 0, 1, 0, 0, 0, 0];
+    uint256[] internal ReccurenceUB = [9998, 11, 31, 6, 23, 59, 59];
 }
